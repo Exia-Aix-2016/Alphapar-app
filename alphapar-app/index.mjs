8de5bbf0313076736100dff2ad826bbebf46f2c0
NooGat <kinghyde111@gmail.com>
2020-12-17 11:05:16 +0100

    Add Denis' beautiful image

--- alphapar-app/index.mjs
+++ alphapar-app/index.mjs
@@ -3,14 +3,19 @@ import session from 'express-session';
 import Keycloak from 'keycloak-connect';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import favicon from 'serve-favicon';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const app = express();
+const memoryStore = new session.MemoryStore();
+const keycloak = new Keycloak({
+  store: memoryStore
+});
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-const memoryStore = new session.MemoryStore();
+app.use(favicon(path.join(__dirname,'public','favicon.ico')));
 
 app.use(
   session({
@@ -23,17 +28,13 @@ app.use(
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-
-const keycloak = new Keycloak({
-  store: memoryStore
-});
-
 app.use(
   keycloak.middleware({
     logout: '/logout',
     admin: '/'
   })
 );
+
 
 app.get('/', function(req, res) {
   res.render(path.join(__dirname+'/about.pug'), { title: "Title", userName: "Jean-Didier", status: "Connected" });
@@ -42,6 +43,7 @@ app.get('/', function(req, res) {
 app.get('/api/user', keycloak.protect('realm:user'), function(req, res) {
   res.sendFile(path.join(__dirname+'/about.pug'));
 });
+
 
 app.listen(3000, err => {
   if (err) {
